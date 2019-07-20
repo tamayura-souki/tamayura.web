e370e174db14421fdadee06a3b4847c300bf5068
tamayura-souki <momentneet@gmail.com>
2019-07-20 19:49:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$('header').load('header.html');
-$('footer').load('footer.html');
-
-alert('aaaaa');
+$(function() {
+    $('header').load('header.html');
+    $('footer').load('footer.html');
+});
